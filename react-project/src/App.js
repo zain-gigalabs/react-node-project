9858001cdc108e76c5259f9901cf855687650b10
zain-gigalabs <zain@gigalabs.co>
2022-05-24 20:20:59 +0500

    practice

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,12 +1,22 @@
-import SignUp from "./components/account/signUp";
+import React, { Suspense } from "react";
+// import SignUp from "./components/account/signUp";
 import Practice from "./components/practice/practice";
 import Hooks from "./components/practice/hooks";
 import ExampleState from "./components/practice/exampleState";
+
+import("./components/practice/advanceGuides").then((math) => {
+  console.log(math.Add(16, 26));
+});
+
+const SignUp = React.lazy(() => import("./components/account/signUp"));
+
 function App() {
   return (
     <>
       <ExampleState>
-        {/* <SignUp/> */}
+        <Suspense fallback={<div>Loading...</div>}>
+          <SignUp />
+        </Suspense>
         {/* <Practice name="class-prop" /> */}
         <Hooks hook="hook-prop" />
       </ExampleState>
--- react-project/src/components/practice/advanceGuides.jsx
+++ react-project/src/components/practice/advanceGuides.jsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export default const Add = (a,b) =>  {
-  return (
-      <>
-      {a+b}
-      </>
-  )
-}
+export const Add = (a, b) => {
+  return a + b;
+};

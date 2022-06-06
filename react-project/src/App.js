3c858eccf02db813842919986ea55e6feec628f5
zain-gigalabs <zain@gigalabs.co>
2022-06-06 15:27:00 +0500

    Update App.js

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,38 +1,11 @@
-// import React, { Suspense } from "react";
-import React from "react";
 import "./App.css";
 
 import SignUp from "./components/account/signUp";
-// import { ValidationSchemaExample } from "./components/practice/formik";
-// import Practice from "./components/practice/practice";
-// import Hooks from "./components/practice/hooks";
-// import ExampleState from "./components/practice/exampleState";
-// import User from "./components/practice/redux/User";
-// import HomeContainer from "./containers/HomeContainer";
-// import HeaderContainer from "./containers/HeaderContainer";
-// import SignUpContainer from "./containers/SignUpContainer";
-// import("./components/practice/advanceGuides").then((math) => {
-//   console.log(math.Add(16, 26));
-// });
-
-// const SignUp = React.lazy(() => import("./components/account/signUp"));
 
 function App() {
   return (
     <>
       <SignUp />
-      {/* <ValidationSchemaExample /> */}
-      {/* <SignUpContainer /> */}
-      {/* <HeaderContainer /> */}
-      {/* <HomeContainer /> */}
-      {/* <User data={{ name: "zain", age: "24" }} /> */}
-      {/* <ExampleState> */}
-      {/* <Suspense fallback={<div>Loading...</div>}>
-          <SignUp />
-        </Suspense> */}
-      {/* <Practice name="class-prop" /> */}
-      {/* <Hooks hook="hook-prop" /> */}
-      {/* </ExampleState> */}
     </>
   );
 }
